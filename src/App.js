e342de5331d9eec68b282a975cefbbf6377579b9
Tsvyatko Ivanov <tsvyatko.ivanov@primeholding.com>
2021-06-10 12:00:43 +0300

    rename to prod name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React STAGING
+          Learn React PROD
         </a>
       </header>
     </div>
