9062b93dd2f6dc4e08a73d1c59ab07001d7623a2
Tsvyatko Ivanov <tsvyatko.ivanov@primeholding.com>
2021-06-10 12:23:36 +0300

    rebase main

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React PROD
+          Learn React STAGING
         </a>
       </header>
     </div>
